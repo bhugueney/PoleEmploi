b7a38fb63f454a12d2685a13a449da0dd63a7022
scientific-coder <scientific-coder@bernard-hugueney.org>
2017-08-30 12:55:33 +0200

    exemple de ServletContextHandler

---
+++ Java_projects/mvnachetypewebapp/src/main/java/co/simplon/PoleEmploi/Application.java
@@ -0,0 +1,35 @@
+package co.simplon.PoleEmploi;
+
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.servlet.*;
+
+import co.simplon.PoleEmploi.HelloServlet;
+
+
+public class Application {
+
+	public static void main(String args[] ) throws Exception{
+	  Server server = new Server(9092);
+	  final ServletContextHandler context = new ServletContextHandler();
+      ServletHandler handler = new ServletHandler();
+      server.setHandler(handler);
+      context.addServlet(DefaultServlet.class, "/*");
+	    //context.addServlet(DefaultServlet.class, "/");
+	    String [] welcomeFiles = {"index.html"};
+	    context.setWelcomeFiles(welcomeFiles);
+	    context.setResourceBase("./src/main/resources/");
+
+	    ServletHolder holderDynamic = new ServletHolder("dynamic", HelloServlet.class);
+        context.addServlet(holderDynamic, "/dynamic/*");
+	    
+	   System.err.println("resourcesBase:"+context.getResourceBase());
+	   server.setHandler(context);
+	  server.setStopAtShutdown(true);
+
+
+      
+      server.start();
+      server.join();
+	}
+	
+}
