63b75a9b00724c12dd172d2c6689f918dfe8f9b7
scientific-coder <scientific-coder@bernard-hugueney.org>
2017-08-30 13:43:20 +0200

    remove unused handler and format

--- Java_projects/mvnachetypewebapp/src/main/java/co/simplon/PoleEmploi/Application.java
+++ Java_projects/mvnachetypewebapp/src/main/java/co/simplon/PoleEmploi/Application.java
@@ -9,27 +9,21 @@ import co.simplon.PoleEmploi.HelloServlet;
 public class Application {
 
 	public static void main(String args[] ) throws Exception{
-	  Server server = new Server(9092);
-	  final ServletContextHandler context = new ServletContextHandler();
-      ServletHandler handler = new ServletHandler();
-      server.setHandler(handler);
-      context.addServlet(DefaultServlet.class, "/*");
-	    //context.addServlet(DefaultServlet.class, "/");
-	    String [] welcomeFiles = {"index.html"};
+		Server server = new Server(9092);
+		final ServletContextHandler context = new ServletContextHandler();
+      	context.addServlet(DefaultServlet.class, "/*");
+      	String [] welcomeFiles = {"index.html"};
 	    context.setWelcomeFiles(welcomeFiles);
 	    context.setResourceBase("./src/main/resources/");
 
 	    ServletHolder holderDynamic = new ServletHolder("dynamic", HelloServlet.class);
         context.addServlet(holderDynamic, "/dynamic/*");
 	    
-	   System.err.println("resourcesBase:"+context.getResourceBase());
-	   server.setHandler(context);
-	  server.setStopAtShutdown(true);
-
-
-      
-      server.start();
-      server.join();
+        System.err.println("resourcesBase:"+context.getResourceBase());
+        server.setHandler(context);
+        server.setStopAtShutdown(true);
+        server.start();
+        server.join();
 	}
 	
 }
